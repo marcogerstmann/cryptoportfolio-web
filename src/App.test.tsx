c06d85a491fcdddae45a908b90aec14a8c574e1a
Marco Gerstmann <marco.gerstmann@flexit.es>
2022-07-17 12:08:36 +0200

    CP-22:chore:remove unnecessary code and files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should render example text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/This is going to be a powerful crypto portfolio app/i);
   expect(linkElement).toBeInTheDocument();
 });
